300543a844027a52c97b2ed181e3fc97930fac6e
Anne <unicornjoshla@gmail.com>
2026-02-23 14:13:52 +0530

    stepclasstask2

---
+++ Stepclasstask.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Stepclasstask {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number % 5 == 0){
+            System.out.println("Divisible by 5");
+        }
+        else{
+            System.out.println("Not Divisible by 5");
+        }
+    }
+
+}
