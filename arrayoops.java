ba39bc03caf8df92f03ca834e866ae8a95f5c3ab
Anne <unicornjoshla@gmail.com>
2026-03-15 22:35:57 +0530

    arrayoops

---
+++ arrayoops.java
@@ -0,0 +1,18 @@
+public class arrayoops {
+    public static void main(String[] args) {
+        String line[] = new String[7];
+        line[0] = String.join("    ***    ","     ***    ","******** ","    *****  "));
+        line[1] = String.join(" **     ** ","  **     ** ","**     **","  **       "));
+        line[2] = String.join("**       **"," **       **","**     **"," **        "));
+        line[3] = String.join("**       **"," **       **","****** **","   *****   "));
+        line[4] = String.join("**       **"," **       **","**       ","        ** "));
+        line[5] = String.join(" **     ** ","  **     ** ","**       ","         **"));
+        line[6] = String.join("    ***    ","     ***    ","**       ","    *****  "));
+       
+        for(int i = 0; i < line.length; i++) {
+            System.out.println(line[i]);
+        }
+
+    }
+    
+}
